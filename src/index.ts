e41367af6785a98851d0ba4b4bc01cc7468f6afb
Dominique Rochefort <dominique.rochefort@hmhco.com>
2019-10-09 21:14:24 -0400

    quote of the day

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
-export * from './components/item-card';
-export * from './components/item-card/item-card-header';
-export * from './components/item-card/item-hints';
-export * from '@hmh-cam/waggle-activity';
-
-// activity components
-export * from '@hmh-cam/option-list';
-export * from '@hmh-cam/response-validation';
-export * from '@hmh-cam/hint-list';
-
-import { RegistryProxy, saveAuthenticationToken } from '@hmh-cam/waggle-activity/dist/client/index';
-
-RegistryProxy.setHostname('http://brproxy.tribalnova.com/waggle-mvp');
-
-// TODO: find proper way to retrieve authentication token
-saveAuthenticationToken('123');
+export * from './components/quote-of-the-day';
