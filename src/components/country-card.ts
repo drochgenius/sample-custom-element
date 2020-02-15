393e266bce13068525b42cbe5d9243ae195b7cb4
Dominique Rochefort <dominique.rochefort@hmhco.com>
2020-02-15 17:26:44 -0500

    move types to typings file

--- src/components/country-card.ts
+++ src/components/country-card.ts
@@ -1,35 +1,5 @@
 import { LitElement, css, CSSResult, html, property, TemplateResult } from 'lit-element';
-
-export interface CountryLanguage {
-    iso639_1: string;
-    iso639_2: string;
-    name: string;
-    nativeName: string;
-}
-
-export interface CountryCurrency {
-    code: string;
-    name: string;
-    symbol: string;
-}
-
-export interface CountryInfo {
-    code: string;
-    alpha2Code: string;
-    alpha3Code: string;
-    name: string; // country name
-    nativeName: string; // country name in own country language
-    population: string; // country population
-    capital: string; // capital city
-    demonym: string; // how to you call people from this country
-    region: string;
-    subregion: string;
-    timezones: string[];
-    languages: CountryLanguage[];
-    area: string;
-}
-
-type CountryData = { [code: string]: CountryInfo };
+import { CountryData, CountryInfo, CountryLanguage } from '../typings';
 
 /**
  * `<quote-of-the-day>`
